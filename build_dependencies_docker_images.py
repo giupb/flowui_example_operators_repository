766f469d3de4c4ee8ff691b25a0ddf55ac34277e
luiz <luiz_tauffer@hotmail.com>
2022-07-04 10:40:40 +0200

    dependencies map

--- build_dependencies_docker_images.py
+++ build_dependencies_docker_images.py
@@ -1,4 +1,7 @@
 from flowuipackage.build_docker_images_operators import build_images_from_code_repository
 
 
-operators_images_map = build_images_from_code_repository(code_repository=".")
+operators_images_map = build_images_from_code_repository(
+    code_repository=".",
+    save_map_as_file=True
+)
