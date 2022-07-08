af0701e9ab59ce5ca7da13a19aedae09ca14c82b
luiz <luiz_tauffer@hotmail.com>
2022-07-08 10:55:10 +0200

    remove old file

--- build_dependencies_docker_images.py
+++
@@ -1,7 +0,0 @@
-from flowuipackage.build_docker_images_operators import build_images_from_code_repository
-
-
-operators_images_map = build_images_from_code_repository(
-    code_repository=".",
-    save_map_as_file=True
-)
